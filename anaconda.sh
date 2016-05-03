c515a7d51329adc7bef6097681d843622fe9926e
Miyazawa Akira <pecorarista@gmail.com>
2016-05-03 18:18:28 +0900

    Update anaconda installer

--- anaconda.sh
+++ anaconda.sh
@@ -1,10 +1,31 @@
 #/bin/bash
-if [ $# -ne 1 ]
+while [[ $# > 1 ]]
+do
+    key="$1"
+    case $key in
+        -p)
+        prefix="$2"
+        shift
+        ;;
+        -v)
+        version="$2"
+        shift
+        ;;
+        *)
+        ;;
+    esac
+    shift
+done
+if [ -z "$prefix" ] || [ -z "$version" ]
 then
-    echo "Usage: anaconda.sh installation-root-directory"
+    echo "$prefix"
+    echo "Usage: anaconda.sh -p <installation-root> -v <2 or 3>"
     exit 1
 fi
 working_dir="$(pwd)"
-cd scala; sbt -Dconfig.file=$working_dir/anaconda.conf "run --prefix=$1"; cd ..
-anaconda_home="$1/anaconda3"
-source "$anaconda_home/bin/activate" root; python -c "import nltk; nltk.download('all')"
+cd scala; \
+    sbt -Dconfig.file=$working_dir/anaconda.conf "run --prefix=$prefix --version=$version"; \
+    cd ..
+anaconda_home="$prefix/anaconda$version"
+source "$anaconda_home/bin/activate" root
+python -c "import nltk; nltk.download('all')"
--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -1,16 +1,12 @@
-import com.typesafe.config.ConfigFactory
-
 import envs.Anaconda
 
 object Main extends App {
 
-  require(args.length == 0 || args.length == 1)
+  require(args.length == 2)
 
-  val prefix = if (args.length == 1) args(0).split('=')(1) else sys.env("HOME")
+  val prefix = args(0).split('=')(1)
+  val version = args(1).split('=')(1).toInt
 
-  (2 to 3).foreach { version =>
-    val install = ConfigFactory.load.getBoolean(s"anacondas.anaconda$version.install")
-    if (install) Anaconda.install(prefix = prefix, version = version)
-  }
+  Anaconda.install(prefix = prefix, version = version)
 
 }
